7856b8f15ac6484b3430ac3a6656afc2ac5db42b
god-changmin <jo22033@gmail.com>
2020-11-24 18:14:13 +0900

    1124start

---
+++ micinfo.py
@@ -0,0 +1,27 @@
+import pyaudio
+import numpy as np
+import peakpicking as pp
+
+
+CHUNK = 2 ** 8
+RATE = 44100
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True,
+                frames_per_buffer=CHUNK, input_device_index=1)
+
+
+
+sample_list=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+pclass= pp.real_time_peak_detection(sample_list,30,100,1)
+#윈도우사이즈,임계값,영향값
+
+while (True):
+    data = np.fromstring(stream.read(CHUNK), dtype=np.int16)
+    if(pclass.thresholding_algo(int(np.average(np.abs(data))))==1):
+        print(np.average(np.abs(data)))
+
+
+stream.stop_stream()
+stream.close()
+p.terminate()
